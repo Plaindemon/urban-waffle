17b0af21bf4fb903c62d16096a9856b9a66fc087
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-11 01:15:35 -0600

    timer added to the page with countdown from click of start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
 //original script file
-var timer = 100;
-var countdown = function(){
+var startTimer = document.getElementById('countdownTimer');
+
+// used examples from the lesson 
+function countdown() {
+    var timer = 100;
+    var timeSetting = setInterval(function(){
     // console.log(timer);
-    timer--;
-    if(timer === 0){
-        console.log("Quiz Time is up")
+    // timer--;
+    if(timer > 1){
+        //
+        startTimer.textContent = timer + ' seconds remain.';
+        timer--;
+    }
+    else if(timer === 0){
+        console.log("Quiz Time is up");
         clearInterval(startCountdown);
-    };
-};
-var startCountdown = setInterval(countdown, 1000);
-//timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
-const startTimer = document.querySelector("timer");
+    }
+    }, 1000);
+}
+
+console.log(countdown);
+// var startCountdown = setInterval(countdown, 1000);
+// //timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
+// // const startTimer = document.querySelector("timer");
 // startTimer.addEventListener('click', function() {
-    //
+    
 
-    //
+    
 
-    //
+    
 // });
 
 // add click event that starts the quiz and then starts the timer
