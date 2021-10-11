72dd44b15ea17e54d61672172e7ac7cafb551641
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-10 18:58:52 -0600

    moved code around and adjusted css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-var timer = 10;
+//original script file
+// grab from the document the h2 within the innerHTML
+document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
+
+//timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
+var timer = 100;
 var countdown = function(){
     console.log(timer);
     timer--;
@@ -7,7 +12,30 @@ var countdown = function(){
         clearInterval(startCountdown);
     };
 };
-
-
-
 var startCountdown = setInterval(countdown, 1000);
+
+
+
+
+
+
+
+document.querySelector('body').style.backgroundColor = 'rgb(166, 165, 167);';
+
+
+
+// const myNodelist = document.querySelectorAll("p");
+// for (let i = 0; i < myNodelist.length; i++) {
+//   myNodelist[i].style.color = "red";
+// }
+
+
+// let element = document.querySelector("button");
+
+// function turnButtonRed (){
+//     element.style.backgroundColor = "#945758";
+//     element.style.color = "white";
+//     element.innerHTML = "Submit"; 
+// }
+
+// element.onclick = turnButtonRed;
