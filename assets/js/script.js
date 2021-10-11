f2fc79a62f4ff7a5238172ba37b6b128a3919252
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-10 20:07:39 -0600

    reworked some css and html to flow better with added Javascript Dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 //original script file
+
+// changes the background color of the document
+document.querySelector('body').style.backgroundColor = 'blue';
+document.querySelector('table').style.backgroundColor = 'orange';
+
+document.querySelector('quizInstruct').innerHTML = 'Quiz questions';
 // grab from the document the h2 within the innerHTML
 document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
+
+
 
 //timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
 var timer = 100;
@@ -14,28 +22,9 @@ var countdown = function(){
 };
 var startCountdown = setInterval(countdown, 1000);
 
+//add quiz questions
 
 
 
 
 
-
-document.querySelector('body').style.backgroundColor = 'rgb(166, 165, 167);';
-
-
-
-// const myNodelist = document.querySelectorAll("p");
-// for (let i = 0; i < myNodelist.length; i++) {
-//   myNodelist[i].style.color = "red";
-// }
-
-
-// let element = document.querySelector("button");
-
-// function turnButtonRed (){
-//     element.style.backgroundColor = "#945758";
-//     element.style.color = "white";
-//     element.innerHTML = "Submit"; 
-// }
-
-// element.onclick = turnButtonRed;
