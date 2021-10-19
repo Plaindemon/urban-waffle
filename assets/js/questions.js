2f00de3a7660b4c997d751cede4ac46da1f9f0a6
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-19 01:03:49 -0600

    messed with code aND BROKE IT

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,6 @@
 //this javascript page is being used to house the questions for the quiz
+
+// this is also an object that each of these can be called later 
 const quizQuestion = [
     {
         question: "What is Ash Ketchums first pokemon?",
@@ -86,5 +88,53 @@ const quizQuestion = [
 // var quizAnswers4 = document.getElementById('quizQChoice4').innerHTML = 'Quiz answer choice 4';
 
 // quizQuestions.addEventListener('')
+
+
+
+// used examples from the lesson 
+function countdown() {
+    var startTimer = document.getElementById('countdownTimer');
+    var timer = 100;
+    var timeSetting = setInterval(function(){
+    // console.log(timer);
+    // timer--;
+    if(timer > 1){
+        //
+        startTimer.textContent = timer + ' seconds remain.';
+        timer--;
+    }
+    else if(timer === 0){
+        console.log("Quiz Time is up");
+        clearInterval(startCountdown);
+    }
+    }, 1000);
+}
+//original script file
+
+
+// add click event that starts the quiz and then starts the timer
+var start = document.getElementById('startbtn');
+console.log(start)
+start.addEventListener('click', 
+countdown)
+
+
+// console.log(countdown);
+// var startCountdown = setInterval(countdown, 1000);
+// //timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
+// // const startTimer = document.querySelector("timer");
+// startTimer.addEventListener('click', function() {
+// });
+
+
+
+
+//
+document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
+
+
+//add quiz questions and connect quiz questions to pop up when the quiz is started
+var questList  = document.getElementById('quizQuestion');
+function ()
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,6 @@
-//original script file
-var startTimer = document.getElementById('countdownTimer');
 
-// used examples from the lesson 
-function countdown() {
-    var timer = 100;
-    var timeSetting = setInterval(function(){
-    // console.log(timer);
-    // timer--;
-    if(timer > 1){
-        //
-        startTimer.textContent = timer + ' seconds remain.';
-        timer--;
-    }
-    else if(timer === 0){
-        console.log("Quiz Time is up");
-        clearInterval(startCountdown);
-    }
-    }, 1000);
-}
+const container = document.getElementById("container")
 
-console.log(countdown);
-// var startCountdown = setInterval(countdown, 1000);
-// //timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
-// // const startTimer = document.querySelector("timer");
-// startTimer.addEventListener('click', function() {
-    
-
-    
-
-    
-// });
-
-// add click event that starts the quiz and then starts the timer
-var start = document.getElementById('startbtn');
-console.log(start)
-start.addEventListener('click', 
-countdown)
-
-
-
-
-
-
-
-
-//
-document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
-
-
-
-
-
-
-
-//add quiz questions and connect quiz questions to pop up when the quiz is started
-
-
-
+container.innerHTML = "<button>Buy!</button>"
 
 
