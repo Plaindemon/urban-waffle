f556c67bc66ca743ca1b0dfcde30d11db2650302
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-10 15:31:44 -0700

    changed some structurasl issues and global varibales

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,144 +1,144 @@
-// variables for creating quiz structure to the page
-const body = document.body;
-const box = document.getElementById("#box");
-let ul = document.createElement("ul");
-ul.setAttribute("class", ".quizAnswerStyle");
-const liA = document.createElement('li');
-const liB = document.createElement('li');
-const liC = document.createElement('li');
-const liD = document.createElement('li');
-const h2 = document.createElement('h2');
+// // variables for creating quiz structure to the page
+// const body = document.body;
+// const box = document.getElementById("#box");
+// let ul = document.createElement("ul");
+// ul.setAttribute("class", ".quizAnswerStyle");
+// const liA = document.createElement('li');
+// const liB = document.createElement('li');
+// const liC = document.createElement('li');
+// const liD = document.createElement('li');
+// const h2 = document.createElement('h2');
 
-const quizQuestions = [
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What type of pokemon is Pikachu?",
-        a: "thunder",
-        b: "lightning",
-        c: "electric",
-        d: "none of the above",
-        correct: "c",
-    },
-    {
-        question: "Which of the following is not a pokemon?",
-        a: "squirtle",
-        b: "gengar",
-        c: "lugio",
-        d: "all of the above",
-        correct: "c",
+// const quizQuestions = [
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What type of pokemon is Pikachu?",
+//         a: "thunder",
+//         b: "lightning",
+//         c: "electric",
+//         d: "none of the above",
+//         correct: "c",
+//     },
+//     {
+//         question: "Which of the following is not a pokemon?",
+//         a: "squirtle",
+//         b: "gengar",
+//         c: "lugio",
+//         d: "all of the above",
+//         correct: "c",
 
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-    {
-        question: "What is Ash Ketchums first pokemon?",
-        a: "pikachu",
-        b: "bulbasaur",
-        c: "charmander",
-        d: "squirtle",
-        correct: "a",
-    },
-];
-// for (let i = 0; i < quizQuestions.valueOf(0).pop(0); i++) {
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+//     {
+//         question: "What is Ash Ketchums first pokemon?",
+//         a: "pikachu",
+//         b: "bulbasaur",
+//         c: "charmander",
+//         d: "squirtle",
+//         correct: "a",
+//     },
+// ];
+// // for (let i = 0; i < quizQuestions.valueOf(0).pop(0); i++) {
 
-//     //add the questions
+// //add the questions
     
 
-//     console.log(quest[0])
-// }
-console.log(quizQuestions[0])
-let quest = "#" + quizQuestions[0];
-// setting the attribute for each question
-h2.innerText = quizQuestions[0];// "QUESTION?";
-console.log(quizQuestions.valueOf(0).pop(0))
-liA.innerText = "Answer A"+  + "Selection";
-liA.setAttribute("id", "A");
+// //     console.log(quest[0])
+// // }
+// console.log(quizQuestions[0])
+// let quest = "#" + quizQuestions[0];
+// // setting the attribute for each question
+// h2.innerText = quizQuestions[0];// "QUESTION?";
+// // console.log(quizQuestions.question)
+// liA.innerText = "Answer A"+  + "Selection";
+// liA.setAttribute("id", "A");
 
-liB.innerText = "Answer B" + "Selection";
-liB.setAttribute("id", "B");
+// liB.innerText = "Answer B" + "Selection";
+// liB.setAttribute("id", "B");
 
-liC.innerText = "Answer C" + "Selection";
-liC.setAttribute("id", "C");
+// liC.innerText = "Answer C" + "Selection";
+// liC.setAttribute("id", "C");
 
-liD.innerText = "Answer D" + "Selection";
-liD.setAttribute("id", "D");
+// liD.innerText = "Answer D" + "Selection";
+// liD.setAttribute("id", "D");
 
-// implementing those var and adding to page 
-ul.style.backgroundColor = "rgba(137, 43, 226, 0.75)";
-ul.append(h2);
-ul.appendChild(liA);
-ul.appendChild(liB);
-ul.appendChild(liC);
-ul.appendChild(liD);
-body.append(ul);
-console.log(ul); // NodeList [<li>]
+// // implementing those var and adding to page 
+// ul.style.backgroundColor = "rgba(137, 43, 226, 0.75)";
+// ul.append(h2);
+// ul.appendChild(liA);
+// ul.appendChild(liB);
+// ul.appendChild(liC);
+// ul.appendChild(liD);
+// body.append(ul);
+// console.log(ul); // NodeList [<li>]
 
 
-console.log(ul.textContent)
-console.log(ul.innerText)
+// console.log(ul.textContent)
+// console.log(ul.innerText)
 
-// li.innerText = "Answer B"
-// li.innerText = "Answer C"
-// li.innerText = "Answer D"
-// let li = document.createElement("li");
-// ul.innerHTML = "<h2>QUESTION?</h2>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>";
+// // li.innerText = "Answer B"
+// // li.innerText = "Answer C"
+// // li.innerText = "Answer D"
+// // let li = document.createElement("li");
+// // ul.innerHTML = "<h2>QUESTION?</h2>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>" + "<br>" + "<li>ANSWERS</li>";
 
-// document.querySelector('ul').innerHTML = "<li>" + quizQuestions "</li>";
-// ul.innerText = "hello world";
-// ul.textContent = "hello world 2";
+// // document.querySelector('ul').innerHTML = "<li>" + quizQuestions "</li>";
+// // ul.innerText = "hello world";
+// // ul.textContent = "hello world 2";
 
-// var quizQuestions = document.getElementById('quizQuestion').innerHTML = 'Quiz questions';
+// // var quizQuestions = document.getElementById('quizQuestion').innerHTML = 'Quiz questions';
 
-// var quizAnswers1 = document.getElementById('quizQChoice1').innerHTML = 'Quiz answer choice 1';
-// var quizAnswers2 = document.getElementById('quizQChoice2').innerHTML = 'Quiz answer choice 2';
-// var quizAnswers3 = document.getElementById('quizQChoice3').innerHTML = 'Quiz answer choice 3';
-// var quizAnswers4 = document.getElementById('quizQChoice4').innerHTML = 'Quiz answer choice 4';
+// // var quizAnswers1 = document.getElementById('quizQChoice1').innerHTML = 'Quiz answer choice 1';
+// // var quizAnswers2 = document.getElementById('quizQChoice2').innerHTML = 'Quiz answer choice 2';
+// // var quizAnswers3 = document.getElementById('quizQChoice3').innerHTML = 'Quiz answer choice 3';
+// // var quizAnswers4 = document.getElementById('quizQChoice4').innerHTML = 'Quiz answer choice 4';
 
-// quizQuestions.addEventListener('')
+// // quizQuestions.addEventListener('')
 
-// const ul = document.querySelector("ul");
+// // const ul = document.querySelector("ul");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,86 @@
 //original script file
 var startTimer = document.getElementById('countdownTimer');
+const box = document.getElementById("box");
+box.style.display = "none";
+var timer = 100;
+var timeSetting;
+var i = 0;
+  
+const quizQuestions = [
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What type of pokemon is Pikachu?",
+        a: "thunder",
+        b: "lightning",
+        c: "electric",
+        d: "none of the above",
+        correct: "c",
+    },
+    {
+        question: "Which of the following is not a pokemon?",
+        a: "squirtle",
+        b: "gengar",
+        c: "lugio",
+        d: "all of the above",
+        correct: "c",
 
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+    {
+        question: "What is Ash Ketchums first pokemon?",
+        a: "pikachu",
+        b: "bulbasaur",
+        c: "charmander",
+        d: "squirtle",
+        correct: "a",
+    },
+];
 //this javascript page is being used to house the questions for the quiz
 // const quizQuestions = [
 //     {
@@ -80,29 +160,30 @@ var startTimer = document.getElementById('countdownTimer');
 
 // used examples from the lesson 
 function countdown() {
-    var timer = 100;
-    var timeSetting = setInterval(function(){
-    // console.log(timer);
-    // timer--;
-    if(timer > 1){
+    box.style.display = "block";
+    timeSetting = setInterval(function(){
+        console.log(timer)
+        if(timer > 1){
         //
         startTimer.textContent = timer + ' seconds remain.';
         timer--;
-    }
-    else if(timer === 0){
+        }
+        else if(timer === 0){
         console.log("Quiz Time is up");
         clearInterval(startCountdown);
-    }
-    }, 1000);
-    timeSetting();
-}
-
+        }
+        }, 1000);
+    questions();
+};
 console.log(countdown);
 // var startCountdown = setInterval(countdown, 1000);
+
+
 // //timer variable that is being used by the countdown variable to create a countdown time from 100 seconds
-// // const startTimer = document.querySelector("timer");
-// startTimer.addEventListener('click', function() {
-// });
+// const startTimer = document.querySelector("timer");
+
+
+
 
 // add click event that starts the quiz and then starts the timer
 var start = document.getElementById('startbtn');
@@ -110,33 +191,82 @@ console.log(start)
 start.addEventListener('click', 
 countdown)
 
-//
-document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
 
-//add quiz questions
 
-// var getQuestions = document.getElementById('#quizQuestion');
 
-// var answers = document.getElementsByClassName('.questAnswer');
-// var  quiz = document.getElement('ul');
-// let quizQuestList = document.querySelector('li#quizQuestion');
-// var quest = quizQuestions.length;
-var quizInstruct = document.getElementById('#quizInstruct');
+// var quizInstruct = document.getElementById('quizInstruct');
+// // if () {
+// //     quizInstruct.classList.remove('boxShow');
+// //     console.log(quizInstruct)
+// //     quizInstruct.classList.change('boxHide');
+    
+// // }
+// //
+// console.log(quizInstruct)
+// document.querySelector('h2').innerHTML = 'CLICK START BUTTON TO BEGIN QUIZ AND START THE TIMER';
+
+
+
 // console.log(quest)
+
+
+// questions();
+// console.log(questions);
+
+//add quiz questions to div container when start button is clicked
 function questions() {
+    // create function to remove the instructions when the quiz start button is clicked
+    // variables for creating quiz structure to the page
+    // const body = document.body;
+    
+    // box.classList.add("boxHide");
+    let ul = document.createElement("ul");
+    ul.classList.add("quizAnswerStyle");
+    const liA = document.createElement('li');
+    const liB = document.createElement('li');
+    const liC = document.createElement('li');
+    const liD = document.createElement('li');
+    const h2 = document.createElement('h2');
+
+    //add the questions
+    // setting the attribute for each question
+    h2.innerText = quizQuestions[i].question;// "QUESTION?";
+    // console.log(quizQuestions.question)
+    liA.innerText = "A:"+ " " + quizQuestions[i].a;
+    liA.setAttribute("id", "A");
+
+    liB.innerText = "B:" + " " + quizQuestions[i].b;
+    liB.setAttribute("id", "B");
+
+    liC.innerText = "C:" + " " + quizQuestions[i].c;
+    liC.setAttribute("id", "C");
+
+    liD.innerText = "D:" + " " + quizQuestions[i].d;
+    liD.setAttribute("id", "D");
+
+    // implementing those var and adding to page 
+    ul.style.backgroundColor = "rgba(137, 43, 226, 0.75)";
+    ul.appendChild(h2);
+    ul.appendChild(liA);
+    ul.appendChild(liB);
+    ul.appendChild(liC);
+    ul.appendChild(liD);
+    console.log(ul); // NodeList [<li>]
+    box.appendChild(ul);
+
+
+    console.log(ul.textContent)
+    console.log(ul.innerText)
+
     // when start button is clicked timer starts and then the quiz questions are cycled through
-    start.addEventListener('click', countdown);
-    for(let i = 0; i < quizQuestions.length; i++){
-        var placeholder = document.querySelector('#quizInstruct');
-        console.log(placeholder);
-    }
-    if (start){
-        quizInstruct.div.remove(quizInstruct);
-    }
-    // console.log(startQuiz);
+    
+    
+    
+        // console.log(startQuiz);
 
 }
 
-// questions();
-console.log(questions);
+// document.querySelector('div #box').innerHTML = "<li>" + quizQuestions "</li>";
 
+
+// document.querySelector('ul').innerHTML = ";
